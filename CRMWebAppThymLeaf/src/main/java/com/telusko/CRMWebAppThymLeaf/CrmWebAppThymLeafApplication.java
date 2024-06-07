9113534340e72f7c45393101ba1b5b1ea1aebad5
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-07 23:32:30 +0530

    Add files via upload

---
+++ CRMWebAppThymLeaf/src/main/java/com/telusko/CRMWebAppThymLeaf/CrmWebAppThymLeafApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.CRMWebAppThymLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmWebAppThymLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmWebAppThymLeafApplication.class, args);
+	}
+
+}
---
+++ CRMWebAppThymLeaf/src/test/java/com/telusko/CRMWebAppThymLeaf/CrmWebAppThymLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.CRMWebAppThymLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmWebAppThymLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
