731af69c18b2b0598a525b3191ac6aaac5c84c0f
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-10 22:25:58 +0530

    Add files via upload

---
+++ springBoot-Thymeleaf-Projects/springBoot-thymeleaf/src/test/java/com/learning/springBoot_thymeleaf/SpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springBoot_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
